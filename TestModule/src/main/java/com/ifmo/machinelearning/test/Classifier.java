a35f6a02bd29876f83110c0ef2d61b782a8dc97c
Arseniy Pendryak <warrior@yandex-team.ru>
2014-09-19 18:53:19 +0400

    added limitation on generic type

--- TestModule/src/main/java/com/ifmo/machinelearning/test/Classifier.java
+++ TestModule/src/main/java/com/ifmo/machinelearning/test/Classifier.java
@@ -3,7 +3,7 @@ package com.ifmo.machinelearning.test;
 /**
  * Created by warrior on 19.09.14.
  */
-public interface Classifier<T> {
+public interface Classifier<T extends ClassifiedData> {
 
     public void training();
 
