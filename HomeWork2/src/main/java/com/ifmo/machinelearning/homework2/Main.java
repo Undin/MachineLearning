6a5858db6e3e5f190b5f4a906f6dfa19fe07553a
Ivan Samborskiy <whiplash1993@yandex.ru>
2014-09-28 19:32:21 +0400

    add samples and Message class

--- HomeWork2/src/main/java/com/ifmo/machinelearning/homework2/Main.java
+++ HomeWork2/src/main/java/com/ifmo/machinelearning/homework2/Main.java
@@ -1,7 +1,24 @@
 package com.ifmo.machinelearning.homework2;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Whiplash on 28.09.2014.
  */
 public class Main {
+
+    private static final File messageDirectory = new File("./Bayes/pu1");
+
+    public static void main(String[] args) throws IOException {
+        List<Message> messages = new ArrayList<>();
+        for (File file : messageDirectory.listFiles()) {
+            if (file.isFile()) {
+                messages.add(new Message(file));
+            }
+        }
+    }
+
 }
--- HomeWork2/src/main/java/com/ifmo/machinelearning/homework2/Message.java
+++ HomeWork2/src/main/java/com/ifmo/machinelearning/homework2/Message.java
@@ -2,14 +2,54 @@ package com.ifmo.machinelearning.homework2;
 
 import com.ifmo.machinelearning.library.ClassifiedData;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
 /**
  * Created by Whiplash on 28.09.2014.
  */
 public class Message implements ClassifiedData {
 
+    private static final String SPAM_MARK = "spmsg";
+
+    private String name;
+    private List<Integer> words;
+    private int classId = -1;
+
+    public Message(File file) throws IOException {
+        name = file.getName();
+        words = new ArrayList<>();
+        classId = name.contains(SPAM_MARK) ? 0 : 1;
+
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        String fileData = "";
+        String line;
+        while ((line = reader.readLine()) != null) {
+            fileData += line;
+        }
+
+        StringTokenizer st = new StringTokenizer(fileData, "Subject: \r\n\t");
+        while (st.hasMoreTokens()) {
+            words.add(Integer.parseInt(st.nextToken()));
+        }
+    }
+
+    public boolean contains(Integer word) {
+        return words.contains(word);
+    }
+
+    public int size() {
+        return words.size();
+    }
+
     @Override
     public int getClassId() {
-        return 0;
+        return classId;
     }
 
     @Override
