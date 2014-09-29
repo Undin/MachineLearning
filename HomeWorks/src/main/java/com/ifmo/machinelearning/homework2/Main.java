39093f80681824957d032152473d68cb8f627313
Arseniy Pendryak <warrior@yandex-team.ru>
2014-09-30 00:04:25 +0400

    ROUNDS = 100

--- HomeWorks/src/main/java/com/ifmo/machinelearning/homework2/Main.java
+++ HomeWorks/src/main/java/com/ifmo/machinelearning/homework2/Main.java
@@ -1,12 +1,11 @@
 package com.ifmo.machinelearning.homework2;
 
+import com.ifmo.machinelearning.graphics.Plot2DBuilder;
 import com.ifmo.machinelearning.test.Statistics;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by Whiplash on 28.09.2014.
@@ -16,16 +15,15 @@ public class Main {
     private static final File messageDirectory = new File("./Bayes/pu1");
 
     private static final int FOLD_NUMBER = 5;
-    private static final int ROUNDS = 20;
+    private static final int ROUNDS = 100;
 
     private static List<Message> sample;
 
     public static void main(String[] args) throws IOException {
         sample = new ArrayList<>();
         getSamples(messageDirectory);
-
         Collections.shuffle(sample);
-        BayesTestMachine testMachine = new BayesTestMachine(sample, true);
+        BayesTestMachine testMachine = new BayesTestMachine(sample, false);
         Statistics statistics = testMachine.crossValidationTest(FOLD_NUMBER, ROUNDS);
         System.out.println(statistics.getFMeasure());
     }
