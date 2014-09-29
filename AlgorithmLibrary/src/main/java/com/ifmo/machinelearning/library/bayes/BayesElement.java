b464f4c098be33a9d37c8d122c807cbb320e47ab
Arseniy Pendryak <warrior@yandex-team.ru>
2014-09-29 23:39:15 +0400

    removed unused code

--- AlgorithmLibrary/src/main/java/com/ifmo/machinelearning/library/bayes/BayesElement.java
+++
@@ -1,9 +0,0 @@
-package com.ifmo.machinelearning.library.bayes;
-
-import com.ifmo.machinelearning.library.ClassifiedData;
-
-/**
- * Created by Whiplash on 28.09.2014.
- */
-public abstract class BayesElement<T> implements ClassifiedData, Iterable<T> {
-}
