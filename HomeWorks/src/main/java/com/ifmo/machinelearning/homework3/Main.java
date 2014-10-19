ec3fa90d5cd58fbcb5dc2338e0b58a323dcf3c28
Ivan Samborskiy <whiplash1993@yandex.ru>
2014-10-19 19:27:30 +0400

    add test system

--- HomeWorks/src/main/java/com/ifmo/machinelearning/homework3/Main.java
+++ HomeWorks/src/main/java/com/ifmo/machinelearning/homework3/Main.java
@@ -1,11 +1,13 @@
 package com.ifmo.machinelearning.homework3;
 
 import com.ifmo.machinelearning.Point;
+import com.ifmo.machinelearning.test.Statistics;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -13,6 +15,9 @@ import java.util.StringTokenizer;
  * Created by Whiplash on 05.10.2014.
  */
 public class Main {
+
+    private static final int FOLD_NUMBER = 5;
+    private static final int ROUNDS = 20;
 
     public static void main(String[] args) throws IOException {
         List<Point> sample = new ArrayList<>();
@@ -25,6 +30,29 @@ public class Main {
             int value = Integer.parseInt(st.nextToken());
             sample.add(new Point(x, y, value));
         }
+
+        Collections.shuffle(sample);
+        List<Point> first = new ArrayList<>(sample.subList(0, sample.size() / 5));
+        List<Point> second = new ArrayList<>(sample.subList(sample.size() / 5, sample.size()));
+
+        SVMTestMachine testMachine = new SVMTestMachine(second, true);
+        testMachine.setKernel(FullPolyKernel.getInstance(3));
+        double neededC = 0;
+        double maxFMeasure = 0;
+        for (int cPow = -5; cPow <= 15; cPow += 2) {
+            double c = StrictMath.pow(2, cPow);
+            testMachine.setC(c);
+            Statistics statistics = testMachine.crossValidationTest(FOLD_NUMBER, ROUNDS);
+            double fMeasure = statistics.getFMeasure();
+            if (maxFMeasure < fMeasure) {
+                maxFMeasure = fMeasure;
+                neededC = c;
+            }
+            System.out.println("F-Measure " + fMeasure + " --- with C = " + c);
+        }
+        testMachine.setC(neededC);
+        Statistics statistics = testMachine.test(first);
+        System.out.println(statistics.getFMeasure());
     }
 
 }
---
+++ HomeWorks/src/main/java/com/ifmo/machinelearning/homework3/SVMTestMachine.java
@@ -0,0 +1,47 @@
+package com.ifmo.machinelearning.homework3;
+
+import com.ifmo.machinelearning.Point;
+import com.ifmo.machinelearning.library.Classifier;
+import com.ifmo.machinelearning.library.Kernel2;
+import com.ifmo.machinelearning.library.svm.SVMClassifier;
+import com.ifmo.machinelearning.test.TestMachine;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by Whiplash on 19.09.2014.
+ */
+public class SVMTestMachine extends TestMachine<Point> {
+
+    private double c = 1;
+    private Kernel2<Point> kernel;
+
+    public SVMTestMachine(List<Point> dataSet) {
+        super(dataSet);
+    }
+
+    public SVMTestMachine(List<Point> dataSet, boolean parallelTest) {
+        super(dataSet, parallelTest);
+    }
+
+    public void setC(double c) {
+        this.c = c;
+    }
+
+    public void setKernel(Kernel2<Point> kernel) {
+        this.kernel = kernel;
+    }
+
+    @Override
+    protected ExecutorService generateExecutorService() {
+        return Executors.newFixedThreadPool(3);
+    }
+
+    @Override
+    protected Classifier<Point> createClassifier(List<Point> dataSet) {
+        return new SVMClassifier<>(dataSet, kernel, c);
+    }
+
+}
