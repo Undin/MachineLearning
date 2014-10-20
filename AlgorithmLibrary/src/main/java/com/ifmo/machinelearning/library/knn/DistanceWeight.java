25158979b8fc7741b34d3e89d6444008c5a0bf15
Arseniy Pendryak <warrior@yandex-team.ru>
2014-10-20 22:06:10 +0400

    removed getInstance() methods

--- AlgorithmLibrary/src/main/java/com/ifmo/machinelearning/library/knn/DistanceWeight.java
+++ AlgorithmLibrary/src/main/java/com/ifmo/machinelearning/library/knn/DistanceWeight.java
@@ -1,35 +1,12 @@
 package com.ifmo.machinelearning.library.knn;
 
 /**
- * Weight function: {@code 1/distance}
- * <p>
  * Created by Whiplash on 19.09.2014.
  */
 public class DistanceWeight implements Weight {
-
-    private static DistanceWeight instance;
-
-    /**
-     * Returns the {@code DistanceWeight} object associated with the current Java application.
-     * Most of the methods of class {@code DistanceWeight} are instance
-     * methods and must be invoked with respect to the current runtime object.
-     *
-     * @return {@code DistanceWeight} object associated with the current
-     * Java application.
-     */
-    public static DistanceWeight getInstance() {
-        if (instance == null) {
-            instance = new DistanceWeight();
-        }
-        return instance;
-    }
-
-    private DistanceWeight() {
-    }
 
     @Override
     public double weight(double distance) {
         return 1 / distance;
     }
-
 }
--- AlgorithmLibrary/src/main/java/com/ifmo/machinelearning/library/knn/KernelWeight.java
+++ AlgorithmLibrary/src/main/java/com/ifmo/machinelearning/library/knn/KernelWeight.java
@@ -3,35 +3,13 @@ package com.ifmo.machinelearning.library.knn;
 import com.ifmo.machinelearning.library.Kernel;
 
 /**
- * Weight function: {@code K(distance)}, where K - kernel function
- * <p>
  * Created by Whiplash on 21.09.2014.
  */
 public class KernelWeight implements Weight {
 
-    private static KernelWeight instance;
-    /**
-     * Kernel function
-     */
     private Kernel kernel;
 
-    /**
-     * Returns the {@code DistanceWeight} object associated with the current Java application.
-     * Most of the methods of class {@code DistanceWeight} are instance
-     * methods and must be invoked with respect to the current runtime object.
-     *
-     * @param kernel {@link #kernel}
-     * @return {@code DistanceWeight} object associated with the current
-     * Java application.
-     */
-    public static KernelWeight getInstance(Kernel kernel) {
-        if (instance == null) {
-            instance = new KernelWeight(kernel);
-        }
-        return instance;
-    }
-
-    private KernelWeight(Kernel kernel) {
+    public KernelWeight(Kernel kernel) {
         this.kernel = kernel;
     }
 
