b65c16224d2c850a4bd338eb93fed92487d36b3a
Arseniy Pendryak <warrior@yandex-team.ru>
2014-10-20 20:43:18 +0400

    make ClassifiedData as interface

--- AlgorithmLibrary/src/main/java/com/ifmo/machinelearning/library/ClassifiedData.java
+++ AlgorithmLibrary/src/main/java/com/ifmo/machinelearning/library/ClassifiedData.java
@@ -1,33 +1,25 @@
 package com.ifmo.machinelearning.library;
 
 /**
- * Abstract class that has the following methods:
+ * Interface that has the following methods:
  * {@link #getClassId()} and {@link #getClassId()}
  * <p>
  * Created by warrior on 19.09.14.
  */
-public abstract class ClassifiedData {
-
-    private int classId = -1;
+public interface ClassifiedData {
 
     /**
      * Returns identifier of class of an element
      *
      * @return class id
      */
-    public int getClassId() {
-        return classId;
-    }
-
-    protected void setClassId(int classId) {
-        this.classId = classId;
-    }
+    public int getClassId();
 
     /**
      * Returns number of class that can return classifier of given element
      *
      * @return number of class
      */
-    public abstract int getClassNumber();
+    public int getClassNumber();
 
 }
