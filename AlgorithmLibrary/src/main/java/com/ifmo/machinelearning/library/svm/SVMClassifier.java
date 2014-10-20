47beb7f7c052e75c3189b1945e17b95441b3fdc9
Arseniy Pendryak <warrior@yandex-team.ru>
2014-10-20 22:39:26 +0400

    modified SVMClassifier

--- AlgorithmLibrary/src/main/java/com/ifmo/machinelearning/library/svm/SVMClassifier.java
+++ AlgorithmLibrary/src/main/java/com/ifmo/machinelearning/library/svm/SVMClassifier.java
@@ -1,8 +1,7 @@
 package com.ifmo.machinelearning.library.svm;
 
-import com.ifmo.machinelearning.library.ClassifiedData;
-import com.ifmo.machinelearning.library.Classifier;
-import com.ifmo.machinelearning.library.Kernel2;
+import com.ifmo.machinelearning.library.AbstractInstanceClassifier;
+import com.ifmo.machinelearning.library.ClassifiedInstance;
 
 import java.util.List;
 import java.util.Random;
@@ -10,7 +9,7 @@ import java.util.Random;
 /**
  * Created by warrior on 19.10.14.
  */
-public class SVMClassifier<T extends ClassifiedData> extends Classifier<T> {
+public class SVMClassifier extends AbstractInstanceClassifier {
 
     private static final int MAX_PASSES = 1;
     private static final double EPS = 10e-5;
@@ -18,36 +17,34 @@ public class SVMClassifier<T extends ClassifiedData> extends Classifier<T> {
     private final Random random = new Random();
 
     protected int[] modifiedClassIds;
-    protected final Kernel2<T> kernelFunction;
+    protected final Kernel kernelFunction;
     protected final double c;
     protected double[] alphas;
     protected double b;
 
-    public SVMClassifier(List<T> data, Kernel2<T> kernelFunction, double c) {
+    public SVMClassifier(List<ClassifiedInstance> data, Kernel kernelFunction, double c) {
         super(data);
         this.kernelFunction = kernelFunction;
         this.c = c;
     }
 
     @Override
-    public Classifier<T> training() {
-        modifiedClassIds = new int[getData().size()];
-        for (int i = 0; i < getData().size(); i++) {
-            int classId = getData().get(i).getClassId();
+    protected void trainingInternal() {
+        modifiedClassIds = new int[size];
+        for (int i = 0; i < size; i++) {
+            int classId = get(i).getClassId();
             modifiedClassIds[i] = classId == 0 ? -1 : classId;
         }
         alphas = smo();
-        return this;
     }
 
     @Override
-    public int getSupposedClassId(T t) {
-        double result = function(t);
+    public int getSupposedClassId(ClassifiedInstance instance) {
+        double result = function(instance);
         return result < 0 ? 0 : 1;
     }
 
     private double[] smo() {
-        int size = getData().size();
         alphas = new double[size];
         b = 0;
         int passes = 0;
@@ -75,8 +72,8 @@ public class SVMClassifier<T extends ClassifiedData> extends Classifier<T> {
                     if (l == h) {
                         continue;
                     }
-                    T xi = getData().get(i);
-                    T xj = getData().get(j);
+                    ClassifiedInstance xi = get(i);
+                    ClassifiedInstance xj = get(j);
                     double n = 2 * kernelFunction.eval(xi, xj) - kernelFunction.eval(xi, xi) - kernelFunction.eval(xj, xj);
                     if (n >= 0) {
                         continue;
@@ -94,7 +91,7 @@ public class SVMClassifier<T extends ClassifiedData> extends Classifier<T> {
                     double b1 = b - ei - modifiedClassIds[i] * (alphas[i] - oldAlphaI) * kernelFunction.eval(xi, xi) -
                                          modifiedClassIds[j] * (alphas[j] - oldAlphaJ) * kernelFunction.eval(xi, xj);
                     double b2 = b - ej - modifiedClassIds[i] * (alphas[i] - oldAlphaI) * kernelFunction.eval(xi, xj) -
-                            modifiedClassIds[j] * (alphas[j] - oldAlphaJ) * kernelFunction.eval(xj, xj);
+                                         modifiedClassIds[j] * (alphas[j] - oldAlphaJ) * kernelFunction.eval(xj, xj);
                     if (alphas[i] > 0 && alphas[i] < c) {
                         b = b1;
                     } else if (alphas[j] > 0 && alphas[j] < c) {
@@ -115,14 +112,14 @@ public class SVMClassifier<T extends ClassifiedData> extends Classifier<T> {
     }
 
     private double calculateE(int i) {
-        return function(getData().get(i)) - modifiedClassIds[i];
+        return function(get(i)) - modifiedClassIds[i];
     }
 
-    protected double function(T t) {
+    protected double function(ClassifiedInstance instance) {
         double result = 0;
-        for (int i = 0; i < getData().size(); i++) {
-            T point = getData().get(i);
-            result += alphas[i] * modifiedClassIds[i] * kernelFunction.eval(point, t);
+        for (int i = 0; i < size; i++) {
+            ClassifiedInstance point = get(i);
+            result += alphas[i] * modifiedClassIds[i] * kernelFunction.eval(point, instance);
         }
         result += b;
         return result;
